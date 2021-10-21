6bb352b12c53d0de1d161a0cd5988a97f2000df1
RoyLowe <roylowe96@gmail.com>
2021-10-20 20:01:20 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '580119439736373',
+        clientSecret: 'a36000fd9e468416760cc5829383b8f9'
+    }
 }
